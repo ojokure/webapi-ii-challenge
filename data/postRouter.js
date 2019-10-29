70c3adaae9f7e0884eba73b803d3ef3b419e52a7
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-29 19:02:02 +0000

    created a delete endpoint to edit posts with specified id

--- data/postRouter.js
+++ data/postRouter.js
@@ -137,4 +137,34 @@ router.delete("/:id", (req, res) => {
       });
     });
 });
+
+router.put("/:id", (req, res) => {
+
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    Posts.update(req.params.id, req.body)
+    .then(updated => {
+      if (!updated) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        })
+      } else {
+        res.status(200).json({
+            updated : req.body
+          }) 
+        }
+
+        })
+    .catch(() => {
+            res.status(500).json({
+              error: "The post information could not be modified."
+            });
+          });
+      }
+  
+  
+});
 module.exports = router;
